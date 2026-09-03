e4a496a2ab7a7afded60371d097994bb6d133e20
FazioNico <contact@nicolasfazio.ch>
2026-09-03 10:40:21 +0200

    done

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import { marketData } from "./market-data";
+
+const filtredMarketData = marketData.filter((market) => {
+  return market.total_volume > 1_000_000;
+});
+document.querySelector("#app").innerHTML = `
+  <h1>Market data (${filtredMarketData.length})</h1>
+  <section>
+    <ul>
+        ${filtredMarketData
+          .map((market) => {
+            return `<li><img width="30px" src="${market.image}" alt="${market.symbol}" /> ${market.symbol}</li>`;
+          })
+          .join("")}
+    </ul>
+  </section>
+`;
