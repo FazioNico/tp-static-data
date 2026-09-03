3f101aba420769cc903a28de328019906f736f76
FazioNico <contact@nicolasfazio.ch>
2026-09-03 12:00:54 +0200

    Refactor market data display to improve structure and add filter functionality

--- src/main.js
+++ src/main.js
@@ -1,18 +1,38 @@
-import './style.css'
+import "./style.css";
 import { marketData } from "./market-data";
 
-const filtredMarketData = marketData.filter((market) => {
+let filtredMarketData = marketData.filter((market) => {
   return market.total_volume > 1_000_000;
 });
+
 document.querySelector("#app").innerHTML = `
-  <h1>Market data (${filtredMarketData.length}/${marketData.length})</h1>
-  <section>
-    <ul>
-        ${filtredMarketData
-          .map((market) => {
-            return `<li><img width="30px" src="${market.image}" alt="${market.symbol}" /> ${market.symbol.toUpperCase()} - $${market.current_price}</li>`;
-          })
-          .join("")}
-    </ul>
-  </section>
+<h1>Market data (${filtredMarketData.length}/${marketData.length})</h1>
+<input type="text" placeholder="BTC, ETH, USDC..." />
+<button>filter</button>
+<section>
+  <ul></ul>
+</section>
 `;
+
+const display = () => {
+  document.querySelector("ul").innerHTML = filtredMarketData
+    .map((market) => {
+      return `<li><img width="30px" src="${market.image}" alt="${market.symbol}" /> ${market.symbol.toUpperCase()} - $${market.current_price}</li>`;
+    })
+    .join("");
+};
+
+display();
+
+// gestion du click sur le button de filtre
+document.querySelector("button").addEventListener("click", () => {
+  // récupérer la valeur de l'input
+  const value = document.querySelector("input").value;
+  // filtrer la liste de donnée de marché
+  filtredMarketData = marketData.filter((market) => {
+    return market.symbol.toLowerCase().includes(value.toLocaleLowerCase());
+  });
+  // afficher la liste dans le html
+  // reset la valeur de l'input
+  display();
+});
