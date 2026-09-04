9853c562315d153db10a9ee565124e8b8cf8fc35
FazioNico <contact@nicolasfazio.ch>
2026-09-04 10:32:26 +0200

    Refactor market data display logic to include detail view and improve filtering

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import "./style.css";
 
 let marketData = [];
 let filtredMarketData = [];
+let selectedItem = null;
 
 const getMarketData = async () => {
   return fetch(
@@ -9,13 +10,42 @@ const getMarketData = async () => {
   ).then((response) => response.json());
 };
 
-const display = () => {
+const displayListItems = () => {
   document.querySelector("ul").innerHTML = filtredMarketData
     .map((market) => {
       return `<li><img width="30px" src="${market.image}" alt="${market.symbol}" /> ${market.symbol.toUpperCase()} - $${market.current_price}</li>`;
     })
     .join("");
 };
+
+const displayListPage = () => {
+  document.querySelector("#app").innerHTML = `
+    <h1>Market data (${filtredMarketData.length}/${marketData.length})</h1>
+    <input type="text" placeholder="BTC, ETH, USDC..." />
+    <button>filter</button>
+    <section>
+      <ul></ul>
+    </section>
+  `;
+}
+
+const displayDetailPage = () => {
+  if (!selectedItem) {
+    alert('Pas de market trouvé');
+    return;
+  }
+  document.querySelector("#app").innerHTML = `
+    <button id="backToList">back to list</button>
+    <h1>${selectedItem.symbol.toUpperCase()}</h1>
+    <section>
+      <ul>
+        ${Object.entries(selectedItem).map(([key, value])=> {
+          return `<li>${key.toUpperCase()}: ${value}</li>`
+        }).join('')}
+      </ul>
+    </section>
+  `;
+}
 
 const runApp = async () => {
   // step 1 : get data from API
@@ -25,14 +55,7 @@ const runApp = async () => {
     return market.total_volume > 1_000_000;
   });
   // step 3 construire le html de base
-  document.querySelector("#app").innerHTML = `
-    <h1>Market data (${filtredMarketData.length}/${marketData.length})</h1>
-    <input type="text" placeholder="BTC, ETH, USDC..." />
-    <button>filter</button>
-    <section>
-      <ul></ul>
-    </section>
-  `;
+  displayListPage();
   // event management with parent element
   document.querySelector('ul').addEventListener('click', (event) => {
     const target = event.target;
@@ -40,10 +63,25 @@ const runApp = async () => {
     const imgElement = liElement.querySelector('img');
     const altAttribute = imgElement.getAttribute('alt');
     const symbol = altAttribute.toUpperCase();
-    alert(symbol);
+    selectedItem = filtredMarketData.find(market => market.symbol.toUpperCase() === symbol);
+    if (!selectedItem) {
+      alert('Pas de market trouvé');
+      return;
+    }
+    console.log(selectedItem)
+    displayDetailPage();
   });
+  // manage back to list event
+  document.querySelector('#app').addEventListener('click', (event)=> {
+    const target = event.target;
+    if (target.id != 'backToList') {
+      return;
+    }
+    displayListPage();
+    displayListItems();
+  })
   // step 4 display real data
-  display();
+  displayListItems();
   // step 5 event management
   // gestion du click sur le button de filtre
   document.querySelector("button").addEventListener("click", () => {
@@ -55,7 +93,7 @@ const runApp = async () => {
     });
     // afficher la liste dans le html
     // reset la valeur de l'input
-    display();
+    displayListItems();
   });
 };
 
